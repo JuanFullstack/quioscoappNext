53f88ac4dfde9b0d0e90da2995576fb42111a361
JuanF.C <omega3031@gmail.com>
2022-12-27 11:11:10 -0300

    Finalizado

--- components/Orden.js
+++ components/Orden.js
@@ -1,17 +1,27 @@
 import Image from "next/image"
 import { formatearDinero } from "../helpers"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 
 function Orden( {orden }) {
 
     const { id , nombre , total , pedido } = orden
-    const completarOrden =()=> {
-        console.log(id)
+    const completarOrden =async ()=> {
+        
+      try {
+       await axios.post(`api/ordenes/${id}`)
+     toast.success("Orden Lista")
+    } catch (error) {
+        toast.error("hubo un error")
+      }
+
+
     }
 
   return (
     <div className=" border p-10 space-y-5" >
-    <h3 className=" text-2xl font-bold" > orden : {id}</h3>  
+    <h3 className=" text-2xl font-bold" > N°orden : {id}</h3>  
     <p className="text-lg font-bold" > cliente : { nombre } </p>
      <div>
 
--- pages/api/ordenes/[id].js
+++ pages/api/ordenes/[id].js
@@ -1,11 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { Toast } from "react-toastify";
 
-export default async function handler(req , res) {
+export default async function handler(req, res) {
+    const prisma = new PrismaClient();
 
+    if (req.method === 'POST') {
+        const {id} = req.query;
 
-if(req.method==="POST"){
-
-console.log("ACTULIZANDO")
-
-}
-
+        const ordenActulizada = await prisma.orden.update({
+            where: {
+                id: parseInt(id),
+            },
+            data: {
+                estado:true
+            },
+        });
+        res.status(200).json(ordenActulizada);
+    }
 }
